9faa11104c6fdd3144bfb8b8f3277082daabc27b
Garima Gupta <ggupta2@uncc.edu>
2026-08-31 19:00:10 -0400

    Export the public workflow API

--- src/applyflow/__init__.py
+++ src/applyflow/__init__.py
@@ -1,4 +1,29 @@
 """Local-first job application workflow automation."""
 
-__all__ = ["__version__"]
+from .models import Activity, Application, ApplicationError, ApplicationStatus
+from .service import (
+    create_application,
+    due_follow_ups,
+    get_application,
+    list_applications,
+    schedule_follow_up,
+    transition_application,
+)
+from .storage import ApplicationStore, StorageError
+
+__all__ = [
+    "Activity",
+    "Application",
+    "ApplicationError",
+    "ApplicationStatus",
+    "ApplicationStore",
+    "StorageError",
+    "create_application",
+    "due_follow_ups",
+    "get_application",
+    "list_applications",
+    "schedule_follow_up",
+    "transition_application",
+    "__version__",
+]
 __version__ = "0.1.0"
