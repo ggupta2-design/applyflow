5bbb8d726ef0366333f0629f04d3f815c042758f
Garima Gupta <ggupta2@uncc.edu>
2026-09-01 18:12:05 -0400

    Export analytics APIs and sync version

--- src/applyflow/__init__.py
+++ src/applyflow/__init__.py
@@ -1,5 +1,12 @@
 """Local-first job application workflow automation."""
 
+from .analytics import (
+    PipelineSummary,
+    StaleApplication,
+    StatusCount,
+    find_stale_applications,
+    summarize_pipeline,
+)
 from .models import Activity, Application, ApplicationError, ApplicationStatus
 from .service import (
     create_application,
@@ -13,6 +20,9 @@ from .storage import ApplicationStore, StorageError
 
 __all__ = [
     "Activity",
+    "PipelineSummary",
+    "StaleApplication",
+    "StatusCount",
     "Application",
     "ApplicationError",
     "ApplicationStatus",
@@ -20,10 +30,12 @@ __all__ = [
     "StorageError",
     "create_application",
     "due_follow_ups",
+    "find_stale_applications",
     "get_application",
     "list_applications",
     "schedule_follow_up",
+    "summarize_pipeline",
     "transition_application",
     "__version__",
 ]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
